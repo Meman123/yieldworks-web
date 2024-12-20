3b2ca39736b65ab46a46e6e3b92eb2ccb39f19e2
Meman123 <87839716+Meman123@users.noreply.github.com>
2024-12-20 15:11:07 -0500

    cambios page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,19 @@
 import client from '../lib/apollo-client';
 import { gql } from '@apollo/client';
 
+type Post = {
+  title: string;
+  content: string;
+};
+
+type PostsData = {
+  posts: {
+    nodes: Post[];
+  };
+};
+
 export default async function Page() {
-  const { data } = await client.query({
+  const { data } = await client.query<PostsData>({
     query: gql`
       query {
         posts {
@@ -19,7 +30,7 @@ export default async function Page() {
     <div>
       <h1>Publicaciones desde WordPress</h1>
       <ul>
-        {data.posts.nodes.map((post, index) => (
+        {data.posts.nodes.map((post: Post, index) => (
           <li key={index}>
             <h2>{post.title}</h2>
             <div dangerouslySetInnerHTML={{ __html: post.content }} />
