c6f57ed416dccf4af461e486666d57555cedcc2a
Meman123 <memanandres1@gmail.com>
2024-12-27 02:39:02 -0500

    .

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true, // Ayuda a detectar errores en el desarrollo
+  webpack: (config) => {
+    config.infrastructureLogging = { level: "verbose" }; // Logs detallados en desarrollo
+    return config;
+  },
 };
 
 export default nextConfig;
