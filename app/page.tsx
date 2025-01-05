e5b3882d660067286b9285a736fd68a3e5fcf1e9
Meman123 <memanandres1@gmail.com>
2025-01-05 00:13:52 -0500

    Test Request meman asdasda

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default async function HomePage() {
     <div className="bg-gray-100 min-h-screen p-4">
       <div className="container mx-auto">
         <h1 className="text-4xl font-bold text-center mb-8 text-gray-800">
-          Publicaciones de WordPress
+          YieldWorks
         </h1>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           {posts.map((post) => (
